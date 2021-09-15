204183fcf2412c3fd9141c2838467f22a07a6371
aizharkyn818 <aizharkyn23@gmail.com>
2021-09-15 15:47:46 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "public/images/dice" + randomNumber1 + ".png");
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src", "public/images/dice" + randomNumber2 + ".png")
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+// Detecting click event
+let activeButton = document.querySelector('.button1');
+activeButton.addEventListener('click', function(){
+  pressedButton();
+  reloadWindow();
+});
+function reloadWindow(){
+  document.location.reload();
+}
+function pressedButton(){
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 150);
+}
+ 
+
+
+
+
+
+
